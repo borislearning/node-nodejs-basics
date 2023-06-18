051ec5c5e96baee7f3a519e382ace099688d5068
Boris Learning <borisr@me.com>
2023-06-18 19:27:58 -0400

    implement delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,25 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'fileToRemove.txt');
+
+    try {
+        await fs.access(filePath);
+
+        await fs.unlink(filePath);
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        } else {
+            throw err;
+        }
+    }
 };
 
-await remove();
+await remove().catch(err => console.error(err.message));
