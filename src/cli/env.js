31f78a2144ad4183e7beac5ce414c74d47bd5721
Boris Learning <borisr@me.com>
2023-06-18 19:33:48 -0400

    implement parseenv function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,20 @@
 const parseEnv = () => {
-    // Write your code here 
+    const args = process.argv.slice(2); // Remove first two default args (node exec path and file path)
+
+    try {
+        for (let i = 0; i < args.length; i += 2) {
+            const propName = args[i].replace('--', '');
+            const value = args[i + 1];
+
+            if (!propName || propName.startsWith('--') || value === undefined) {
+                throw new Error('Invalid command-line arguments');
+            }
+
+            console.log(`${propName} is ${value}`);
+        }
+    } catch (err) {
+        console.error(err.message);
+    }
 };
 
 parseEnv();
