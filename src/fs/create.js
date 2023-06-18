5cb2ec7410acd412a4b6198dde2601c2010478f7
Boris Learning <borisr@me.com>
2023-06-18 18:50:31 -0400

    implement create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,25 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'files', 'fresh.txt');
+
+    try {
+        await fs.access(filePath);
+        throw new Error('FS operation failed');
+    }
+    catch (err) {
+        if (err.code === 'ENOENT') {
+            await fs.writeFile(filePath, 'I am fresh and young');
+        } else {
+            throw err;
+        }
+    }
 };
 
-await create();
+await create().catch(err => console.error(err.message));
