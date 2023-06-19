558129c9051a371508bde218163487c672310c05
Boris Learning <borisr@me.com>
2023-06-18 22:52:37 -0400

    fix env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,10 +1,12 @@
-const parseEnv = () => {
-    for (const key in process.env) {
-        if (key.startsWith('RSS_')) {
-            console.log(`${key}=${process.env[key]}`);
-        }
+function parseArgs() {
+    const args = process.argv.slice(2); // Remove node call and file name from args
+
+    for (let i = 0; i < args.length; i += 2) {
+        const propName = args[i].replace('--', '');
+        const value = args[i + 1];
+
+        console.log(`${propName} is ${value}`);
     }
 }
 
-
-parseEnv();
+parseArgs();
