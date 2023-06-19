5b0fe1edf03ae87539eaf920216f37b2e6745dfa
Boris Learning <borisr@me.com>
2023-06-18 22:55:34 -0400

    try catch errors in parseArgs f

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,11 +1,15 @@
 function parseArgs() {
-    const args = process.argv.slice(2); // Remove node call and file name from args
+    try {
+        const args = process.argv.slice(2);
 
-    for (let i = 0; i < args.length; i += 2) {
-        const propName = args[i].replace('--', '');
-        const value = args[i + 1];
+        for (let i = 0; i < args.length; i += 2) {
+            const propName = args[i].replace('--', '');
+            const value = args[i + 1];
 
-        console.log(`${propName} is ${value}`);
+            console.log(`${propName} is ${value}`);
+        }
+    } catch (error) {
+        console.error('opperation failed:', error);
     }
 }
 
