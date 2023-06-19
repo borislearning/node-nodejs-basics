37b1d6c4665cbcf62797ecffe10c16b060924d0a
Boris Learning <borisr@me.com>
2023-06-19 00:30:18 -0400

    child processes

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,25 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const workerPath = path.join(__dirname, 'files', 'script.js');
+
+import { spawn } from 'child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = spawn('node', [workerPath, ...args], {
+        stdio: [process.stdin, process.stdout, 'pipe', 'ipc']
+    });
+
+    child.stderr.on('data', (data) => {
+        console.error(`Child process stderr: ${data}`);
+    });
+
+    child.on('close', (code) => {
+        console.log(`Child process exited with code ${code}`);
+    });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['arg1', 'arg2', 'arg3']);
