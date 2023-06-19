7b5bfbde9f04c7825feede996ff12a92d55419f5
Boris Learning <borisr@me.com>
2023-06-18 22:45:26 -0400

    fix parse env function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,20 +1,10 @@
 const parseEnv = () => {
-    const args = process.argv.slice(2); // Remove first two default args (node exec path and file path)
-
-    try {
-        for (let i = 0; i < args.length; i += 2) {
-            const propName = args[i].replace('--', '');
-            const value = args[i + 1];
-
-            if (!propName || propName.startsWith('--') || value === undefined) {
-                throw new Error('Invalid command-line arguments');
-            }
-
-            console.log(`${propName} is ${value}`);
+    for (const key in process.env) {
+        if (key.startsWith('RSS_')) {
+            console.log(`${key}=${process.env[key]}`);
         }
-    } catch (err) {
-        console.error(err.message);
     }
-};
+}
+
 
 parseEnv();
