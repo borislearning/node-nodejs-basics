2632b22243df9cec83107d90032accd56e1185ca
Boris Learning <borisr@me.com>
2023-06-18 23:43:32 -0400

    implement read and write async. fix list path

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,25 @@
+import { createReadStream } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const readStream = createReadStream(filePath);
+        readStream.on('data', (chunk) => {
+            process.stdout.write(chunk);
+        });
+
+        readStream.on('error', (error) => {
+            console.error('An error occurred while reading the file:', error);
+        });
+    } catch (error) {
+        console.error('An error occurred:', error);
+    }
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,29 @@
+import { createWriteStream } from 'fs';
+import { promises as fs } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = path.join(__dirname, '/files', 'filetoWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+    try {
+        const writeStream = createWriteStream(filePath);
+
+        process.stdin.pipe(writeStream);
+
+        writeStream.on('error', (error) => {
+            console.error('An error occurred while writing to the file:', error);
+        });
+
+        process.stdin.on('end', () => {
+            writeStream.end();
+        });
+    } catch (error) {
+        console.error('An error occurred:', error);
+    }
 };
 
 await write();
